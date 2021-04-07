f1665de0292e88a4dc8d02d21e454980f556c044
joannsiciliano <joannsiciliano@gmail.com>
2021-04-07 13:51:32 -0600

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+class EyesOnMe extends React.Component {
+  onFocus = () => console.log("Good!");
+  onBlur = () => console.log("Hey! Eyes on me!");
+
+  render() {
+    return (
+      <div class="eyesOnMe">
+        <button onBlur={this.onBlur} onFocus={this.onFocus}></button>
+      </div>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
+class Keypad extends React.Component {
+  handleInputPW = () => console.log("Entering Password");
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.handleInputPW}></input>
+      </div>
+    );
+  }
+}
+
+export default Keypad;
